8adce1d38bde0de0181bf1a4af2a4f6c79f766a3
SHUlpt <1424526265@qq.com>
2022-02-28 15:41:58 +0800

    修改模型导入语法, 避免Pycharm中报错

--- models/UMEAN/test.py
+++ models/UMEAN/test.py
@@ -2,7 +2,8 @@ from data import MatrixDataset
 from utils.evaluation import mae, mse, rmse
 from utils.model_util import freeze_random
 
-from .model import UMEANModel
+from models.UMEAN.model import UMEANModel
+
 """
 RESULT UMEAN:
 Density:0.05,type:rt,mae:0.8816043906403586,mse:3.4499158310187976,rmse:1.8573949044343794
@@ -19,7 +20,6 @@ Density:0.2,type:tp,mae:53.95651903024888,mse:12114.249782573803,rmse:110.064752
 freeze_random()  # 冻结随机数 保证结果一致
 
 for density in [0.05, 0.1, 0.15, 0.2]:
-
     type_ = "tp"
     rt_data = MatrixDataset(type_)
     train_data, test_data = rt_data.split_train_test(density)

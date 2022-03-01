ac6b0df43c1c8039d1e31407d91316c8e097bf83
SHUlpt <1424526265@qq.com>
2022-03-01 21:50:32 +0800

    测试结果保存到日志

--- models/IPCC/test.py
+++ models/IPCC/test.py
@@ -1,8 +1,12 @@
 from data import MatrixDataset
 from utils.evaluation import mae, mse, rmse
-from utils.model_util import freeze_random
-
 from model import IPCCModel
+# 冻结随机数
+from utils.model_util import freeze_random
+# 日志
+from utils.mylogger import TNLog
+# 多进程
+import multiprocessing as mp
 
 """
 RESULT IPCC:
@@ -10,6 +14,9 @@ Density:0.05, topk:200, type:rt, mae:0.8413, mse:3.6662, rmse:1.9147
 """
 
 freeze_random()  # 冻结随机数 保证结果一致
+
+logger = TNLog('IPCCModel')
+logger.initial_logger()
 
 for density in [0.05, 0.1, 0.15, 0.2]:
     type_ = "rt"
@@ -19,11 +26,11 @@ for density in [0.05, 0.1, 0.15, 0.2]:
     imean = IPCCModel()
     imean.fit(train_data, metric='PCC')
 
-    topk = 200
-    y, y_pred = imean.predict(test_data, topk)
+    for topk in [50, 100, 200, 500]:
+        y, y_pred = imean.predict(test_data, topk)
 
-    mae_ = mae(y, y_pred)
-    mse_ = mse(y, y_pred)
-    rmse_ = rmse(y, y_pred)
+        mae_ = mae(y, y_pred)
+        mse_ = mse(y, y_pred)
+        rmse_ = rmse(y, y_pred)
 
-    print(f"Density:{density}, topk:{topk}, type:{type_}, mae:{mae_:.4f}, mse:{mse_:.4f}, rmse:{rmse_:.4f}")
+        logger.info(f"Density:{density}, topk:{topk}, type:{type_}, mae:{mae_:.4f}, mse:{mse_:.4f}, rmse:{rmse_:.4f}")

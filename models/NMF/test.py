760d39bd27cfa4fca760273eaf7fddac8031079e
TD21forever <435212619@qq.com>
2022-03-09 15:06:32 +0800

    修改了一下NMF

--- models/NMF/test.py
+++ models/NMF/test.py
@@ -1,12 +1,12 @@
 from data import MatrixDataset
-from utils.evaluation import mae, mse, rmse
-from utils.model_util import freeze_random
-
-from model import NMFModel
-# 日志
-from utils.mylogger import TNLog
 # Non-negative Matrix Factorization
 from sklearn.decomposition import NMF
+from utils.evaluation import mae, mse, rmse
+from utils.model_util import freeze_random
+# 日志
+from utils.mylogger import TNLog
+
+from .model import NMFModel
 
 """
 RESULT NMF:
@@ -24,15 +24,29 @@ for density in [0.05, 0.1, 0.15, 0.2]:
 
     # latent_dim = 50
     # epochs = 1000
-    for latent_dim in range(2, 31, 4):
-        for epochs in range(10, 100, 20):
-            nmf = NMFModel(md_data.row_n, md_data.col_n, latent_dim)
-            nmf.fit(train_data, test_data, epochs, verbose=False)
-            y, y_pred = nmf.predict(test_data)
+    # for latent_dim in range(2, 31, 4):
+    #     for epochs in range(10, 100, 20):
+    #         nmf = NMFModel(md_data.row_n, md_data.col_n, latent_dim)
+    #         nmf.fit(train_data, test_data, epochs, verbose=False)
+    #         y, y_pred = nmf.predict(test_data)
 
-            mae_ = mae(y, y_pred)
-            mse_ = mse(y, y_pred)
-            rmse_ = rmse(y, y_pred)
+    #         mae_ = mae(y, y_pred)
+    #         mse_ = mse(y, y_pred)
+    #         rmse_ = rmse(y, y_pred)
 
-            logger.info(
-                f"Density:{density:.02f}, type:{type_}, latent_dim:{latent_dim:{3}}, epochs:{epochs:{4}}, mae:{mae_:.04f}, mse:{mse_:.04f}, rmse:{rmse_:.04f}")
+    #         logger.info(
+    #             f"Density:{density:.02f}, type:{type_}, latent_dim:{latent_dim:{3}}, epochs:{epochs:{4}}, mae:{mae_:.04f}, mse:{mse_:.04f}, rmse:{rmse_:.04f}")
+
+    latent_dim = 12
+    epochs = 1000
+    nmf = NMFModel(md_data.row_n, md_data.col_n, latent_dim)
+    nmf.fit(train_data, test_data, epochs, verbose=True, normalize=False,early_stop=True)
+    y, y_pred = nmf.predict(test_data)
+
+    mae_ = mae(y, y_pred)
+    mse_ = mse(y, y_pred)
+    rmse_ = rmse(y, y_pred)
+
+    logger.info(
+        f"Density:{density:.02f}, type:{type_}, latent_dim:{latent_dim:{3}}, epochs:{epochs:{4}}, mae:{mae_:.04f}, mse:{mse_:.04f}, rmse:{rmse_:.04f}"
+    )

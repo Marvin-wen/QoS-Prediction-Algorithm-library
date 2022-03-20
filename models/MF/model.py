28e2f0dab6757e6ebe65134099ec2dd4daf1fb0b
SHUlpt <1424526265@qq.com>
2022-03-20 10:20:23 +0800

    MF模型代码优化

--- models/MF/model.py
+++ models/MF/model.py
@@ -1,28 +1,28 @@
 import copy
-
 import numpy as np
 from tqdm import tqdm
 from utils.evaluation import mae, mse, rmse
 
 
 class MFModel(object):
+    """FunkSVD
+    """
+
     def __init__(self, n_user, n_item, latent_dim, lr, lambda_) -> None:
         super().__init__()
         self.lr = lr
         self.lambda_ = lambda_
-        self.latent_dim = latent_dim
+        self.latent_dim = latent_dim  # 隐特征维度
         self.n_user = n_user
         self.n_item = n_item
         self.user_vec = None
         self.item_vec = None
 
     def _init_vec(self):
-        """初始化用户和物品的特征矩阵
+        """初始化用户和项目的特征矩阵
         """
-        self.user_vec = 2 * np.random.random(
-            (self.n_user, self.latent_dim)) - 1
-        self.item_vec = 2 * np.random.random(
-            (self.n_item, self.latent_dim)) - 1
+        self.user_vec = 2 * np.random.random((self.n_user, self.latent_dim)) - 1
+        self.item_vec = 2 * np.random.random((self.n_item, self.latent_dim)) - 1
 
     def fit(self, traid, test, epochs=100, verbose=True, early_stop=True):
         if not self.user_vec and not self.item_vec:
@@ -37,32 +37,31 @@ class MFModel(object):
                 user_idx, item_idx, y = int(row[0]), int(row[1]), float(row[2])
                 # 计算预测值
                 y_pred = self.user_vec[user_idx] @ self.item_vec[item_idx].T
+                # if np.isnan(y_pred).any():
+                #     print('error')
                 e_ui = y - y_pred
                 # 计算用户梯度
-                user_grad = -2 * e_ui * self.item_vec[
-                    item_idx] + 2 * self.lambda_ * self.user_vec[user_idx]
-                # 计算物品梯度
-                item_grad = -2 * e_ui * self.user_vec[
-                    user_idx] + 2 * self.lambda_ * self.item_vec[item_idx]
+                user_grad = -2 * e_ui * self.item_vec[item_idx] + 2 * self.lambda_ * self.user_vec[user_idx]
+                # 计算项目梯度
+                item_grad = -2 * e_ui * self.user_vec[user_idx] + 2 * self.lambda_ * self.item_vec[item_idx]
                 # 梯度更新
                 self.user_vec[user_idx] -= self.lr * user_grad
                 self.item_vec[item_idx] -= self.lr * item_grad
 
             if early_stop and np.mean(np.abs(self.user_vec - tmp_user_vec)) < 1e-4 and \
-                np.mean(np.abs(self.item_vec - tmp_item_vec)) < 1e-4:
+                    np.mean(np.abs(self.item_vec - tmp_item_vec)) < 1e-4:
                 print('Converged')
                 break
 
             if verbose and (epoch + 1) % 10 == 0:
                 y_list, y_pred_list = self.predict(test)
-                print(f"[{epoch}/{epochs}] MAE:{mae(y_list,y_pred_list):.5f}")
+                print(f"[{epoch + 1}/{epochs}] MAE:{mae(y_list, y_pred_list):.5f}")
 
     def predict(self, traid):
-        assert isinstance(self.user_vec,
-                          np.ndarray), "please fit first e.g. model.fit()"
+        assert self.user_vec is not None, "Please fit first e.g. model.fit()"
         y_pred_list = []
         y_list = []
-        for row in tqdm(traid):
+        for row in tqdm(traid, desc='MF Perdicting: '):
             uid, iid, y = int(row[0]), int(row[1]), float(row[2])
             y_pred = self.user_vec[uid] @ self.item_vec[iid].T
             y_pred_list.append(y_pred)
--- models/MF/test.py
+++ models/MF/test.py
@@ -2,30 +2,38 @@ from data import MatrixDataset
 from utils.evaluation import mae, mse, rmse
 from utils.model_util import freeze_random
 
-from .model import MFModel
+from model import MFModel
+# 日志
+from utils.mylogger import TNLog
+
 """
 RESULT MF:
-
+Density:0.05, type:rt, mae:0.6717, mse:2.4526, rmse:1.5661
+Density:0.10, type:rt, mae:0.5169, mse:1.7230, rmse:1.3126
+Density:0.15, type:rt, mae:0.4725, mse:1.5054, rmse:1.2269
+Density:0.20, type:rt, mae:0.4396, mse:1.3708, rmse:1.1708
 """
 
 freeze_random()  # 冻结随机数 保证结果一致
 
-for density in [0.05, 0.1, 0.15, 0.2]:
+logger = TNLog('MF')
+logger.initial_logger()
 
-    type_ = "rt"
+for density in [0.05, 0.1, 0.15, 0.2]:
+    type_ = "tp"
     latent_dim = 8
-    lr = 0.001
-    lambda_ = 0.1
+    lr = 0.0001
+    lambda_ = 0.05
     epochs = 200
     md_data = MatrixDataset(type_)
     train_data, test_data = md_data.split_train_test(density)
 
     mf = MFModel(md_data.row_n, md_data.col_n, latent_dim, lr, lambda_)
-    mf.fit(train_data, test_data, epochs)
+    mf.fit(train_data, test_data, epochs, verbose=False)
     y, y_pred = mf.predict(test_data)
 
     mae_ = mae(y, y_pred)
     mse_ = mse(y, y_pred)
     rmse_ = rmse(y, y_pred)
 
-    print(f"Density:{density},type:{type_},mae:{mae_},mse:{mse_},rmse:{rmse_}")
+    logger.info(f"Density:{density:.02f}, type:{type_}, mae:{mae_:.04f}, mse:{mse_:.04f}, rmse:{rmse_:.04f}")

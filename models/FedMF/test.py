9a8750b393e5b84d549659f175d1fcc718801cb8
chenmo <chenmo@shu.edu.cn>
2022-03-22 20:23:40 +0800

    update FedMF

--- models/FedMF/test.py
+++ models/FedMF/test.py
@@ -4,28 +4,37 @@ from utils.model_util import freeze_random
 
 from . import Clients, Server
 from .model import FedMF
+
+from utils.mylogger import TNLog
+
 """
 RESULT FedMF: 
 1000epoch
-Density:0.05,type:rt,mae:0.6090604947629363,mse:2.3006280931616536,rmse:1.5167821508580768
-Density:0.1,type:rt,mae:0.5071641017174705,mse:1.7203263210368958,rmse:1.3116121076891962
-Density:0.15,type:rt,mae:0.46475316376452325,mse:1.4854062714808631,rmse:1.2187724445034287
+Density:0.05,type:rt,mae:0.5949614110683237,mse:2.1892629584652785,rmse:1.4796158144820157
+Density:0.1,type:rt,mae:0.5065232463307372,mse:1.6816029233716594,rmse:1.2967663333737731
+Density:0.15,type:rt,mae:0.466420129810779,mse:1.4792783323010215,rmse:1.216255866296653
 Density:0.2,type:rt,mae:0.43765304163567553,mse:1.3690546770840173,rmse:1.1700660994508034
 
+# 1 FedMF 优化后的结果
 
+
+# 2 FedMF TP结果
 
 """
+
+logger = TNLog('FedMF')
+logger.initial_logger()
 
 freeze_random()  # 冻结随机数 保证结果一致
 
 for density in [0.05, 0.1, 0.15, 0.2]:
 
     # 1
-    type_ = "rt"
+    type_ = "tp"
     latent_dim = 8
     lr = 0.0001
     lambda_ = 0.1
-    epochs = 1000
+    epochs = 3000
 
     # 2
     # type_ = "rt"
@@ -48,4 +57,4 @@ for density in [0.05, 0.1, 0.15, 0.2]:
     mse_ = mse(y, y_pred)
     rmse_ = rmse(y, y_pred)
 
-    print(f"Density:{density},type:{type_},mae:{mae_},mse:{mse_},rmse:{rmse_}")
+    logger.info(f"Density:{density},type:{type_},mae:{mae_},mse:{mse_},rmse:{rmse_}")
